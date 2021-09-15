fbc9a3a8075b025d662f4a98b20b42bc1f424197
Hugo Cerda <hu.cerdaj@alumnos.duoc.cl>
2021-09-14 23:49:26 -0300

    -

--- TeLlevoApp/src/app/restablecer/restablecer.page.ts
+++ TeLlevoApp/src/app/restablecer/restablecer.page.ts
@@ -1,4 +1,7 @@
+import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-restablecer',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restablecer.page.scss'],
 })
 export class RestablecerPage implements OnInit {
+  dato: String;
+  constructor(
+    public ToastController: ToastController,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  restablecer() {
+    this.presentToast();
+    this.router.navigate(['/login']);
   }
 
+  async presentToast() {
+    const toast = await this.ToastController.create({
+      message: 'Correo de restablecimiento enviado a ' + this.dato,
+      duration: 2000,
+    });
+    toast.present();
+  }
 }
