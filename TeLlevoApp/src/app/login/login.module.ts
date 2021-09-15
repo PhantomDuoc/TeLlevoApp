2492cc4b26d68cf6d533146306bd32b7815b7ea6
Hugo Cerda Jara <73355079+PhantomDuoc@users.noreply.github.com>
2021-09-15 00:21:23 -0300

    intentos

--- TeLlevoApp/src/app/login/login.module.ts
+++ TeLlevoApp/src/app/login/login.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import {MatSliderModule} from '@angular/material/slider'
 
 @NgModule({
   imports: [
@@ -14,7 +15,7 @@ import { LoginPage } from './login.page';
     FormsModule,
     IonicModule,
     LoginPageRoutingModule,
-    ReactiveFormsModule
+    MatSliderModule
   ],
   declarations: [LoginPage]
 })
