7db1e15d8d9596becdaeda07d20ad1d2694e20a0
Hugo Cerda Jara <73355079+PhantomDuoc@users.noreply.github.com>
2022-01-29 18:39:58 -0300

    ultimo?

--- TeLlevoApp/e2e/src/app.e2e-spec.ts
+++ TeLlevoApp/e2e/src/app.e2e-spec.ts
@@ -1,3 +1,4 @@
+import { browser } from 'protractor';
 import { AppPage } from './app.po';
 
 describe('new App', () => {
@@ -7,7 +8,8 @@ describe('new App', () => {
     page = new AppPage();
   });
 
-  it('should be blank', () => {
+  it('should be blank', async () => {
+    await browser.waitForAngularEnabled(false);
     page.navigateTo();
     expect(page.getParagraphText()).toContain('Start with Ionic UI Components');
   });
